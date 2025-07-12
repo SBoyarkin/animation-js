d53bc2767eee3d49a62eba704501f14f436270a9
SBoyarkin <sergey.boyarkin@mail.ru>
2025-07-12 21:27:13 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./js/app";
 
 const root = document.querySelector(".root");
 const lorem =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, atque aut beatae delectus distinctio, dolore ea eligendi excepturi explicabo illo itaque iure labore libero minus nobis pariatur temporibus veritatis voluptatibus?";
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, atque aut beatae delectus distinctio," +
+  " dolore ea eligendi excepturi explicabo illo itaque iure labore libero minus nobis pariatur temporibus veritatis voluptatibus?";
 const collapse = new Collapse(root);
 collapse.create(lorem);
--- src/js/collapse.js
+++ src/js/collapse.js
@@ -10,6 +10,26 @@ export class Collapse {
     btn.classList.add("btn");
     btn.textContent = "Collapse";
 
+    btn.addEventListener("click", () => {
+      const isActive = data.classList.contains("active");
+
+      if (isActive) {
+        data.classList.add("inactive");
+        data.classList.remove("active");
+
+        data.addEventListener(
+          "animationend",
+          () => {
+            data.classList.remove("inactive");
+          },
+          { once: true },
+        );
+      } else {
+        // Просто раскрываем
+        data.classList.add("active");
+      }
+    });
+
     const data = document.createElement("div");
     data.classList.add("collapse-text");
     data.textContent = text;
